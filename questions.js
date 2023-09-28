90b8563a6eb6955591c7f6b1a99795cea1ce0b39
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-09-28 18:35:37 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    question: "Em um sítio há 12 árvores. Cada árvore possui 12 galhos e em cada galho tem 12 maçãs. Quantas maçãs existem no sítio?",
+    answers: [
+      { option: "1728", correct: true },
+      { option: "144", correct: false },
+      { option: "1564", correct: false },
+    ],
+  },
+  {
+    question: "Em um projeto para a construção de um cinema, os arquitetos estão avaliando a relação entre a quantidade de fileiras e a quantidade de cadeiras em cada fileira. O projeto inicial prevê uma sala para 304 pessoas. No caso de utilizarem 19 fileiras, o número de cadeiras por fileira será: ",
+    answers: [
+      { option: "15", correct: false },
+      { option: "16", correct: true },
+      { option: "12", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 10 x 8?",
+    answers: [
+      { option: "800", correct: false },
+      { option: "90", correct: false },
+      { option: "80", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 7 x 7?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+    ],
+  },
+
+  {
+    question: "O valor da expressão 20x3 + 2x2y5, para x = - 4 e y = 2 é: ",
+    answers: [
+      { option: "400", correct: false },
+      { option: "256", correct: false },
+      { option: "-256", correct: true },
+    ],
+  },
+];
